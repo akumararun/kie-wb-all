kiegroup/jbpm-wb

JBPM-4379 add IdentityProviderAwareProcessListenerProducer for process instance can get initiator.

jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-backend/src/main/java/org/jbpm/console/ng/pr/backend/server/listener/IdentityProviderAwareProcessListenerProducer.java
@@ -1,0 +1,23 @@
+package org.jbpm.console.ng.pr.backend.server.listener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jbpm.kie.services.impl.IdentityProviderAwareProcessListener;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.runtime.manager.EventListenerProducer;
+import org.jbpm.runtime.manager.api.qualifiers.Process;
+@Process
+public class IdentityProviderAwareProcessListenerProducer implements EventListenerProducer<IdentityProviderAwareProcessListener> {
+
+    @Override
+    public List<IdentityProviderAwareProcessListener> getEventListeners(String identifier,
+                                                                        Map<String, Object> params) {
+        List<IdentityProviderAwareProcessListener> identityProviderAwareProcessListenerList =  new ArrayList<IdentityProviderAwareProcessListener>();
+        IdentityProviderAwareProcessListener identityProviderAwareProcessListener = new IdentityProviderAwareProcessListener( (KieSession)params.get( "ksession" ) );
+        identityProviderAwareProcessListenerList.add( identityProviderAwareProcessListener );
+        return identityProviderAwareProcessListenerList;
+    }
+
+}

akumararun/kie-wb-all

jbpm-console-ng/jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-backend/src/main/java/org/jbpm/console/ng/pr/backend/server/listener/IdentityProviderAwareProcessListenerProducer.java
@@ -1,0 +1,25 @@
+package org.jbpm.console.ng.pr.backend.server.listener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jbpm.kie.services.impl.IdentityProviderAwareProcessListener;
+import org.jbpm.runtime.manager.api.qualifiers.Process;
+import org.kie.api.event.process.ProcessEventListener;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.runtime.manager.EventListenerProducer;
+@Process
+public class IdentityProviderAwareProcessListenerProducer implements EventListenerProducer<ProcessEventListener> {
+
+    @Override
+    public List<ProcessEventListener> getEventListeners(String identifier,
+                                                                        Map<String, Object> params) {
+        
+        List<ProcessEventListener> identityProviderAwareProcessListenerList =  new ArrayList<ProcessEventListener>();
+        InitiatorProviderProcessListener initiatorProviderProcessListener = new InitiatorProviderProcessListener( (KieSession)params.get( "ksession" ) );
+        identityProviderAwareProcessListenerList.add( initiatorProviderProcessListener );
+        return identityProviderAwareProcessListenerList;
+    }
+
+}