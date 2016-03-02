kiegroup/jbpm-wb

JBPM-4958 and BZ-1160144 Update process instances and tasks search
Allow searching by task id and process instance id if search criteria is only numbers
Search for process definition id using exact match

jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-client/src/main/java/org/jbpm/console/ng/ht/client/editors/taskslist/grid/dash/DataSetTasksListGridPresenter.java
@@ -16,10 +16,8 @@
 package org.jbpm.console.ng.ht.client.editors.taskslist.grid.dash;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Observes;
@@ -51,8 +49,6 @@ import org.jbpm.console.ng.gc.client.experimental.grid.base.ExtendedPagedTable;
 import org.jbpm.console.ng.gc.client.list.base.AbstractListView.ListView;
 import org.jbpm.console.ng.gc.client.list.base.AbstractScreenListPresenter;
 import org.jbpm.console.ng.gc.client.list.base.events.SearchEvent;
-import org.uberfire.ext.widgets.common.client.menu.RefreshMenuBuilder;
-import org.uberfire.ext.widgets.common.client.menu.RefreshSelectorMenuBuilder;
 import org.jbpm.console.ng.gc.client.menu.RestoreDefaultFiltersMenuBuilder;
 import org.jbpm.console.ng.ht.client.editors.quicknewtask.QuickNewTaskPopup;
 import org.jbpm.console.ng.ht.client.i18n.Constants;
@@ -64,6 +60,8 @@ import org.uberfire.client.annotations.WorkbenchPartView;
 import org.uberfire.client.annotations.WorkbenchScreen;
 import org.uberfire.client.mvp.UberView;
 import org.uberfire.client.workbench.widgets.common.ErrorPopupPresenter;
+import org.uberfire.ext.widgets.common.client.menu.RefreshMenuBuilder;
+import org.uberfire.ext.widgets.common.client.menu.RefreshSelectorMenuBuilder;
 import org.uberfire.mvp.Command;
 import org.uberfire.paging.PageResponse;
 import org.uberfire.workbench.model.menu.MenuFactory;
@@ -157,21 +155,15 @@ public class DataSetTasksListGridPresenter extends AbstractScreenListPresenter<T
                         dataSetQueryHelper.setLastSortOrder( SortOrder.ASCENDING );
                     }
 
-                    if ( textSearchStr != null && textSearchStr.trim().length() > 0 ) {
-
-                        DataSetFilter filter = new DataSetFilter();
-                        List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
-                        filters.add( likeTo( COLUMN_NAME, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filters.add( likeTo( COLUMN_DESCRIPTION, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filters.add( likeTo( COLUMN_PROCESSID, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filter.addFilterColumn( OR( filters ) );
-
-                        if ( currentTableSettings.getDataSetLookup().getFirstFilterOp() != null ) {
-                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn( OR( filters ) );
+                    final List<ColumnFilter> filters = getColumnFilters(textSearchStr);
+                    if (filters.isEmpty() == false) {
+                        if (currentTableSettings.getDataSetLookup().getFirstFilterOp() != null) {
+                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn(OR(filters));
                         } else {
-                            currentTableSettings.getDataSetLookup().addOperation( filter );
+                            final DataSetFilter filter = new DataSetFilter();
+                            filter.addFilterColumn(OR(filters));
+                            currentTableSettings.getDataSetLookup().addOperation(filter);
                         }
-                        textSearchStr = "";
                     }
                     dataSetQueryHelper.setDataSetHandler(currentTableSettings);
                     dataSetQueryHelper.lookupDataSet(visibleRange.getStart(), createDataSetTaskCallback(visibleRange.getStart(), currentTableSettings));
@@ -181,6 +173,21 @@ public class DataSetTasksListGridPresenter extends AbstractScreenListPresenter<T
             errorPopup.showMessage(Constants.INSTANCE.UnexpectedError(e.getMessage()) );
         }
 
+    }
+
+    protected List<ColumnFilter> getColumnFilters(final String searchString) {
+        final List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
+        if (searchString != null && searchString.trim().length() > 0) {
+            try {
+                final Long taskId = Long.valueOf(searchString.trim());
+                filters.add(equalsTo(COLUMN_TASKID, taskId));
+            } catch (NumberFormatException ex) {
+                filters.add(likeTo(COLUMN_NAME, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_DESCRIPTION, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_PROCESSID, "%" + searchString.toLowerCase() + "%", false));
+            }
+        }
+        return filters;
     }
 
     protected DataSetReadyCallback createDataSetTaskCallback(final int startRange, final FilterSettings tableSettings){

jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-client/src/test/java/org/jbpm/console/ng/ht/client/editors/taskslist/grid/dash/DataSetTasksListGridPresenterTest.java
@@ -28,6 +28,7 @@ import org.dashbuilder.dataset.DataSet;
 import org.dashbuilder.dataset.DataSetLookup;
 import org.dashbuilder.dataset.DataSetOp;
 import org.dashbuilder.dataset.client.DataSetReadyCallback;
+import org.dashbuilder.dataset.filter.ColumnFilter;
 import org.dashbuilder.dataset.filter.DataSetFilter;
 import org.dashbuilder.dataset.sort.SortOrder;
 import org.jbpm.console.ng.df.client.filter.FilterSettings;
@@ -252,6 +253,53 @@ public class DataSetTasksListGridPresenterTest {
 
         verify(viewMock).applyFilterOnPresenter(anyString());
         assertEquals(searchEvent.getFilter(), presenter.getTextSearchStr());
+    }
+
+    @Test
+    public void testSearchFilterEmpty() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterNull() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(null);
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterEmptyTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("     ");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterId() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("1");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_TASKID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterIdTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(" 1 ");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_TASKID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterString() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("taskName");
+
+        assertEquals(3, filters.size());
+        assertEquals(COLUMN_NAME, filters.get(0).getColumnId());
+        assertEquals(COLUMN_DESCRIPTION, filters.get(1).getColumnId());
+        assertEquals(COLUMN_PROCESSID, filters.get(2).getColumnId());
     }
 
 }

jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/instance/list/variables/dash/DataSetProcessInstanceWithVariablesListPresenter.java
@@ -46,11 +46,10 @@ import org.jbpm.console.ng.gc.client.experimental.grid.base.ExtendedPagedTable;
 import org.jbpm.console.ng.gc.client.list.base.AbstractListView.ListView;
 import org.jbpm.console.ng.gc.client.list.base.AbstractScreenListPresenter;
 import org.jbpm.console.ng.gc.client.list.base.events.SearchEvent;
-import org.uberfire.ext.widgets.common.client.menu.RefreshMenuBuilder;
-import org.uberfire.ext.widgets.common.client.menu.RefreshSelectorMenuBuilder;
 import org.jbpm.console.ng.gc.client.menu.RestoreDefaultFiltersMenuBuilder;
 import org.jbpm.console.ng.pr.client.editors.instance.signal.ProcessInstanceSignalPresenter;
 import org.jbpm.console.ng.pr.client.i18n.Constants;
+import org.jbpm.console.ng.pr.client.perspectives.DataSetProcessInstancesWithVariablesPerspective;
 import org.jbpm.console.ng.pr.forms.client.editors.quicknewinstance.QuickNewProcessInstancePopup;
 import org.jbpm.console.ng.pr.model.ProcessInstanceSummary;
 import org.jbpm.console.ng.pr.model.events.NewProcessInstanceEvent;
@@ -65,6 +64,8 @@ import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.client.mvp.UberView;
 import org.uberfire.client.workbench.widgets.common.ErrorPopupPresenter;
 import org.uberfire.ext.widgets.common.client.common.popups.errors.ErrorPopup;
+import org.uberfire.ext.widgets.common.client.menu.RefreshMenuBuilder;
+import org.uberfire.ext.widgets.common.client.menu.RefreshSelectorMenuBuilder;
 import org.uberfire.lifecycle.OnFocus;
 import org.uberfire.lifecycle.OnOpen;
 import org.uberfire.lifecycle.OnStartup;
@@ -177,22 +178,16 @@ public class DataSetProcessInstanceWithVariablesListPresenter extends AbstractSc
                         dataSetQueryHelper.setLastSortOrder( SortOrder.ASCENDING );
                     }
 
-                    if ( textSearchStr != null && textSearchStr.trim().length() > 0 ) {
-
-                        DataSetFilter filter = new DataSetFilter();
-                        List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
-                        filters.add( likeTo( COLUMN_PROCESSNAME, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filters.add( likeTo( COLUMN_PROCESSINSTANCEDESCRIPTION, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filters.add( likeTo( COLUMN_IDENTITY, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filter.addFilterColumn( OR( filters ) );
-
-                        if ( currentTableSettings.getDataSetLookup().getFirstFilterOp() != null ) {
-                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn( OR( filters ) );
+                    final List<ColumnFilter> filters = getColumnFilters(textSearchStr);
+                    if (filters.isEmpty() == false) {
+                        if (currentTableSettings.getDataSetLookup().getFirstFilterOp() != null) {
+                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn(OR(filters));
                         } else {
-                            currentTableSettings.getDataSetLookup().addOperation( filter );
+                            final DataSetFilter filter = new DataSetFilter();
+                            filter.addFilterColumn(OR(filters));
+                            currentTableSettings.getDataSetLookup().addOperation(filter);
                         }
                     }
-
 
                     dataSetQueryHelper.setCurrentTableSettings( currentTableSettings );
                     dataSetQueryHelper.setDataSetHandler( currentTableSettings );
@@ -204,6 +199,22 @@ public class DataSetProcessInstanceWithVariablesListPresenter extends AbstractSc
         } catch ( Exception e ) {
             errorPopup.showMessage(Constants.INSTANCE.UnexpectedError(e.getMessage()));
         }
+    }
+
+    protected List<ColumnFilter> getColumnFilters(final String searchString) {
+        final List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
+        if (searchString != null && searchString.trim().length() > 0) {
+            try {
+                final Long instanceId = Long.valueOf(searchString.trim());
+                filters.add(equalsTo(COLUMN_PROCESSINSTANCEID, instanceId));
+            } catch (NumberFormatException ex) {
+                filters.add(equalsTo(COLUMN_PROCESSID, searchString));
+                filters.add(likeTo(COLUMN_PROCESSNAME, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_PROCESSINSTANCEDESCRIPTION, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_IDENTITY, "%" + searchString.toLowerCase() + "%", false));
+            }
+        }
+        return filters;
     }
 
     protected DataSetReadyCallback createDataSetDomainSpecificCallback( final int startRange, final int totalRowSize, final List<ProcessInstanceSummary> instances, final FilterSettings tableSettings ) {
@@ -360,7 +371,7 @@ public class DataSetProcessInstanceWithVariablesListPresenter extends AbstractSc
 
     @OnOpen
     public void onOpen() {
-        this.textSearchStr = place.getParameter("processName", "");
+        this.textSearchStr = place.getParameter(DataSetProcessInstancesWithVariablesPerspective.PROCESS_ID, "");
         refreshGrid();
     }
 

jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/test/java/org/jbpm/console/ng/pr/client/editors/instance/list/variables/dash/DataSetProcessInstanceWithVariablesListPresenterTest.java
@@ -31,6 +31,7 @@ import org.dashbuilder.dataset.DataSet;
 import org.dashbuilder.dataset.DataSetLookup;
 import org.dashbuilder.dataset.DataSetOp;
 import org.dashbuilder.dataset.client.DataSetReadyCallback;
+import org.dashbuilder.dataset.filter.ColumnFilter;
 import org.dashbuilder.dataset.filter.DataSetFilter;
 import org.dashbuilder.dataset.sort.SortOrder;
 import org.jbpm.console.ng.bd.service.KieSessionEntryPoint;
@@ -337,6 +338,54 @@ public class DataSetProcessInstanceWithVariablesListPresenterTest {
 
         verify(viewMock).applyFilterOnPresenter(anyString());
         assertEquals(searchEvent.getFilter(), presenter.getTextSearchStr());
+    }
+
+    @Test
+    public void testSearchFilterEmpty() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterNull() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(null);
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterEmptyTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("     ");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterId() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("1");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_PROCESSINSTANCEID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterIdTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(" 1 ");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_PROCESSINSTANCEID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterString() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("processName");
+
+        assertEquals(4, filters.size());
+        assertEquals(COLUMN_PROCESSID, filters.get(0).getColumnId());
+        assertEquals(COLUMN_PROCESSNAME, filters.get(1).getColumnId());
+        assertEquals(COLUMN_PROCESSINSTANCEDESCRIPTION, filters.get(2).getColumnId());
+        assertEquals(COLUMN_IDENTITY, filters.get(3).getColumnId());
     }
 
 }

akumararun/kie-wb-all

jbpm-console-ng/jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-client/src/main/java/org/jbpm/console/ng/ht/client/editors/taskslist/grid/dash/DataSetTasksListGridPresenter.java
@@ -155,21 +155,15 @@ public class DataSetTasksListGridPresenter extends AbstractScreenListPresenter<T
                         dataSetQueryHelper.setLastSortOrder( SortOrder.ASCENDING );
                     }
 
-                    if ( textSearchStr != null && textSearchStr.trim().length() > 0 ) {
-
-                        DataSetFilter filter = new DataSetFilter();
-                        List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
-                        filters.add(likeTo(COLUMN_NAME, "%" + textSearchStr.toLowerCase() + "%", false));
-                        filters.add(likeTo(COLUMN_DESCRIPTION, "%" + textSearchStr.toLowerCase() + "%", false));
-                        filters.add(likeTo(COLUMN_PROCESS_ID, "%" + textSearchStr.toLowerCase() + "%", false));
-                        filter.addFilterColumn( OR( filters ) );
-
-                        if ( currentTableSettings.getDataSetLookup().getFirstFilterOp() != null ) {
-                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn( OR( filters ) );
+                    final List<ColumnFilter> filters = getColumnFilters(textSearchStr);
+                    if (filters.isEmpty() == false) {
+                        if (currentTableSettings.getDataSetLookup().getFirstFilterOp() != null) {
+                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn(OR(filters));
                         } else {
-                            currentTableSettings.getDataSetLookup().addOperation( filter );
+                            final DataSetFilter filter = new DataSetFilter();
+                            filter.addFilterColumn(OR(filters));
+                            currentTableSettings.getDataSetLookup().addOperation(filter);
                         }
-                        textSearchStr = "";
                     }
                     dataSetQueryHelper.setDataSetHandler(currentTableSettings);
                     dataSetQueryHelper.lookupDataSet(visibleRange.getStart(), createDataSetTaskCallback(visibleRange.getStart(), currentTableSettings));
@@ -179,6 +173,21 @@ public class DataSetTasksListGridPresenter extends AbstractScreenListPresenter<T
             errorPopup.showMessage(Constants.INSTANCE.UnexpectedError(e.getMessage()) );
         }
 
+    }
+
+    protected List<ColumnFilter> getColumnFilters(final String searchString) {
+        final List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
+        if (searchString != null && searchString.trim().length() > 0) {
+            try {
+                final Long taskId = Long.valueOf(searchString.trim());
+                filters.add(equalsTo(COLUMN_TASK_ID, taskId));
+            } catch (NumberFormatException ex) {
+                filters.add(likeTo(COLUMN_NAME, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_DESCRIPTION, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_PROCESS_ID, "%" + searchString.toLowerCase() + "%", false));
+            }
+        }
+        return filters;
     }
 
     protected DataSetReadyCallback createDataSetTaskCallback(final int startRange, final FilterSettings tableSettings){

jbpm-console-ng/jbpm-console-ng-human-tasks/jbpm-console-ng-human-tasks-client/src/test/java/org/jbpm/console/ng/ht/client/editors/taskslist/grid/dash/DataSetTasksListGridPresenterTest.java
@@ -28,6 +28,7 @@ import org.dashbuilder.dataset.DataSet;
 import org.dashbuilder.dataset.DataSetLookup;
 import org.dashbuilder.dataset.DataSetOp;
 import org.dashbuilder.dataset.client.DataSetReadyCallback;
+import org.dashbuilder.dataset.filter.ColumnFilter;
 import org.dashbuilder.dataset.filter.DataSetFilter;
 import org.dashbuilder.dataset.sort.SortOrder;
 import org.jbpm.console.ng.df.client.filter.FilterSettings;
@@ -252,6 +253,53 @@ public class DataSetTasksListGridPresenterTest {
 
         verify(viewMock).applyFilterOnPresenter(anyString());
         assertEquals(searchEvent.getFilter(), presenter.getTextSearchStr());
+    }
+
+    @Test
+    public void testSearchFilterEmpty() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterNull() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(null);
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterEmptyTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("     ");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterId() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("1");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_TASK_ID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterIdTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(" 1 ");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_TASK_ID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterString() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("taskName");
+
+        assertEquals(3, filters.size());
+        assertEquals(COLUMN_NAME, filters.get(0).getColumnId());
+        assertEquals(COLUMN_DESCRIPTION, filters.get(1).getColumnId());
+        assertEquals(COLUMN_PROCESS_ID, filters.get(2).getColumnId());
     }
 
 }

jbpm-console-ng/jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/main/java/org/jbpm/console/ng/pr/client/editors/instance/list/variables/dash/DataSetProcessInstanceWithVariablesListPresenter.java
@@ -46,11 +46,10 @@ import org.jbpm.console.ng.gc.client.experimental.grid.base.ExtendedPagedTable;
 import org.jbpm.console.ng.gc.client.list.base.AbstractListView.ListView;
 import org.jbpm.console.ng.gc.client.list.base.AbstractScreenListPresenter;
 import org.jbpm.console.ng.gc.client.list.base.events.SearchEvent;
-import org.uberfire.ext.widgets.common.client.menu.RefreshMenuBuilder;
-import org.uberfire.ext.widgets.common.client.menu.RefreshSelectorMenuBuilder;
 import org.jbpm.console.ng.gc.client.menu.RestoreDefaultFiltersMenuBuilder;
 import org.jbpm.console.ng.pr.client.editors.instance.signal.ProcessInstanceSignalPresenter;
 import org.jbpm.console.ng.pr.client.i18n.Constants;
+import org.jbpm.console.ng.pr.client.perspectives.DataSetProcessInstancesWithVariablesPerspective;
 import org.jbpm.console.ng.pr.forms.client.editors.quicknewinstance.QuickNewProcessInstancePopup;
 import org.jbpm.console.ng.pr.model.ProcessInstanceSummary;
 import org.jbpm.console.ng.pr.model.events.NewProcessInstanceEvent;
@@ -65,6 +64,8 @@ import org.uberfire.client.mvp.PlaceManager;
 import org.uberfire.client.mvp.UberView;
 import org.uberfire.client.workbench.widgets.common.ErrorPopupPresenter;
 import org.uberfire.ext.widgets.common.client.common.popups.errors.ErrorPopup;
+import org.uberfire.ext.widgets.common.client.menu.RefreshMenuBuilder;
+import org.uberfire.ext.widgets.common.client.menu.RefreshSelectorMenuBuilder;
 import org.uberfire.lifecycle.OnFocus;
 import org.uberfire.lifecycle.OnOpen;
 import org.uberfire.lifecycle.OnStartup;
@@ -177,22 +178,16 @@ public class DataSetProcessInstanceWithVariablesListPresenter extends AbstractSc
                         dataSetQueryHelper.setLastSortOrder( SortOrder.ASCENDING );
                     }
 
-                    if ( textSearchStr != null && textSearchStr.trim().length() > 0 ) {
-
-                        DataSetFilter filter = new DataSetFilter();
-                        List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
-                        filters.add( likeTo(COLUMN_PROCESS_NAME, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filters.add( likeTo(COLUMN_PROCESS_INSTANCE_DESCRIPTION, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filters.add( likeTo(COLUMN_IDENTITY, "%" + textSearchStr.toLowerCase() + "%", false ) );
-                        filter.addFilterColumn( OR( filters ) );
-
-                        if ( currentTableSettings.getDataSetLookup().getFirstFilterOp() != null ) {
-                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn( OR( filters ) );
+                    final List<ColumnFilter> filters = getColumnFilters(textSearchStr);
+                    if (filters.isEmpty() == false) {
+                        if (currentTableSettings.getDataSetLookup().getFirstFilterOp() != null) {
+                            currentTableSettings.getDataSetLookup().getFirstFilterOp().addFilterColumn(OR(filters));
                         } else {
-                            currentTableSettings.getDataSetLookup().addOperation( filter );
+                            final DataSetFilter filter = new DataSetFilter();
+                            filter.addFilterColumn(OR(filters));
+                            currentTableSettings.getDataSetLookup().addOperation(filter);
                         }
                     }
-
 
                     dataSetQueryHelper.setCurrentTableSettings( currentTableSettings );
                     dataSetQueryHelper.setDataSetHandler( currentTableSettings );
@@ -204,6 +199,22 @@ public class DataSetProcessInstanceWithVariablesListPresenter extends AbstractSc
         } catch ( Exception e ) {
             errorPopup.showMessage(Constants.INSTANCE.UnexpectedError(e.getMessage()));
         }
+    }
+
+    protected List<ColumnFilter> getColumnFilters(final String searchString) {
+        final List<ColumnFilter> filters = new ArrayList<ColumnFilter>();
+        if (searchString != null && searchString.trim().length() > 0) {
+            try {
+                final Long instanceId = Long.valueOf(searchString.trim());
+                filters.add(equalsTo(COLUMN_PROCESS_INSTANCE_ID, instanceId));
+            } catch (NumberFormatException ex) {
+                filters.add(equalsTo(COLUMN_PROCESS_ID, searchString));
+                filters.add(likeTo(COLUMN_PROCESS_NAME, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_PROCESS_INSTANCE_DESCRIPTION, "%" + searchString.toLowerCase() + "%", false));
+                filters.add(likeTo(COLUMN_IDENTITY, "%" + searchString.toLowerCase() + "%", false));
+            }
+        }
+        return filters;
     }
 
     protected DataSetReadyCallback createDataSetDomainSpecificCallback( final int startRange, final int totalRowSize, final List<ProcessInstanceSummary> instances, final FilterSettings tableSettings ) {
@@ -360,7 +371,7 @@ public class DataSetProcessInstanceWithVariablesListPresenter extends AbstractSc
 
     @OnOpen
     public void onOpen() {
-        this.textSearchStr = place.getParameter("processName", "");
+        this.textSearchStr = place.getParameter(DataSetProcessInstancesWithVariablesPerspective.PROCESS_ID, "");
         refreshGrid();
     }
 

jbpm-console-ng/jbpm-console-ng-process-runtime/jbpm-console-ng-process-runtime-client/src/test/java/org/jbpm/console/ng/pr/client/editors/instance/list/variables/dash/DataSetProcessInstanceWithVariablesListPresenterTest.java
@@ -31,6 +31,7 @@ import org.dashbuilder.dataset.DataSet;
 import org.dashbuilder.dataset.DataSetLookup;
 import org.dashbuilder.dataset.DataSetOp;
 import org.dashbuilder.dataset.client.DataSetReadyCallback;
+import org.dashbuilder.dataset.filter.ColumnFilter;
 import org.dashbuilder.dataset.filter.DataSetFilter;
 import org.dashbuilder.dataset.sort.SortOrder;
 import org.jbpm.console.ng.bd.service.KieSessionEntryPoint;
@@ -337,6 +338,54 @@ public class DataSetProcessInstanceWithVariablesListPresenterTest {
 
         verify(viewMock).applyFilterOnPresenter(anyString());
         assertEquals(searchEvent.getFilter(), presenter.getTextSearchStr());
+    }
+
+    @Test
+    public void testSearchFilterEmpty() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterNull() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(null);
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterEmptyTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("     ");
+
+        assertTrue(filters.isEmpty());
+    }
+
+    @Test
+    public void testSearchFilterId() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("1");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_PROCESS_INSTANCE_ID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterIdTrim() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters(" 1 ");
+
+        assertEquals(1, filters.size());
+        assertEquals(COLUMN_PROCESS_INSTANCE_ID, filters.get(0).getColumnId());
+    }
+
+    @Test
+    public void testSearchFilterString() {
+        final List<ColumnFilter> filters = presenter.getColumnFilters("processName");
+
+        assertEquals(4, filters.size());
+        assertEquals(COLUMN_PROCESS_ID, filters.get(0).getColumnId());
+        assertEquals(COLUMN_PROCESS_NAME, filters.get(1).getColumnId());
+        assertEquals(COLUMN_PROCESS_INSTANCE_DESCRIPTION, filters.get(2).getColumnId());
+        assertEquals(COLUMN_IDENTITY, filters.get(3).getColumnId());
     }
 
 }